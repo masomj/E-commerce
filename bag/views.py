00201b77d4989e88bb551983256a9931f82cbe2c
masomj <masonjayjones@gmail.com>
2022-10-23 11:22:07 +0100

    updated shopping bag template

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def view_bag(request):
     
     return render(request, 'bag/bag.html')
 
+
+def add_to_bag(request, item_id):
+    """Add product to bag"""
+    
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag',{})
+    
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity                
+    bag[item_id] = quantity
+    
+    request.session['bag'] = bag
+ 
+    return redirect(redirect_url)

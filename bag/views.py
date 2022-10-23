a1fb1c4349cb05f7ca7b53a5cf03f6ecee225e7d
masomj <masonjayjones@gmail.com>
2022-10-23 17:02:17 +0100

    completed size functionality

--- bag/views.py
+++ bag/views.py
@@ -13,12 +13,25 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag',{})
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size: 
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys(): #if item already in bag, add item Id, size + quantity to dictionary
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size':{size:quantity}} #if not already in bag, create dictionary of item, so 1 item ID can hold multiple size entries 'item by size : xl:1'
+            
     else:
-        bag[item_id] = quantity                
-    bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity                
+        bag[item_id] = quantity
     
     request.session['bag'] = bag
  
